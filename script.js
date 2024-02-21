4bb58894f551b95f7dae702d8575542c39f8ba78
usernameB99 <kum-a-saufn@live.at>
2024-02-21 18:04:13 +0100

    add delete buttons to listed items

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
@@ -10,34 +9,49 @@ console.log(leadsFromLocalStorage);
 
 const tabBtn = document.getElementById("tab-btn");
 
-
 if (leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage;
     render(myLeads);
 }
 
-
 tabBtn.addEventListener("click", function(){
-
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        
         myLeads.push(tabs[0].url);
         localStorage.setItem("myLeads", JSON.stringify(myLeads));
         render(myLeads);
     })
-
 })
 
 function render(leads){
     let listItems = "";
     for (let i = 0; i < leads.length; i++){
-        listItems += `<li>
-                          <a href='${leads[i]}' target='_blank'>
-                              ${leads[i]}
-                          </a>
-                      </li>`;
+        if (leads[i].includes("http://") || leads[i].includes("https://")) {
+            listItems += `<li>
+                            <button class="delete-item-btn" id="${i}">
+                            <a href='${leads[i]}' target='_blank'>${leads[i]}</a>
+                            <span class="material-symbols-outlined">delete</span>
+                            </button>
+                          </li>`;
+        } else {
+            listItems += `<li>
+                            <button class="delete-item-btn" id="${i}">
+                            <span class="material-symbols-outlined">delete</span>
+                            </button>
+                             ${leads[i]}
+                          </li>`;
+        }
     }
     ulEl.innerHTML = listItems;
+
+    let deleteButtons = document.getElementsByClassName("delete-item-btn");
+    for (let i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener("click", function(){
+            let id = this.getAttribute("id");
+            myLeads.splice(id, 1);
+            localStorage.setItem("myLeads", JSON.stringify(myLeads));
+            render(myLeads);
+        })
+    }
 }
 
 deleteBtn.addEventListener("dblclick", function(){
@@ -56,7 +70,3 @@ inputBtn.addEventListener("click", function() {
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     render(myLeads);
 })
-
-
-
-
